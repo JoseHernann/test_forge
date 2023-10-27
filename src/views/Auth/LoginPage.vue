9164a0a77d66c7f2426c2ca7ca9ff50731a7af12
Alejandra Orozco <nancy.palomerao@uanl.mx>
2023-10-27 11:48:25 -0600

    Agregar verificación de campos vacíos en el inicio de sesión y modificar base.css
    -Se ha agregado una verificación para asegurarse de que los campos de correo y contraseña no estén vacíos antes de intentar iniciar sesión. Si alguno de los campos está vacío, se muestra un mensaje de error que indica al usuario que complete ambos campos. Además, se han realizado modificaciones en el archivo `base.css` para mejorar el estilo y la apariencia de la aplicación.

--- src/views/Auth/LoginPage.vue
+++ src/views/Auth/LoginPage.vue
@@ -6,14 +6,17 @@ const email = ref('')
 const password = ref('')
 const error = ref('')
 
-const route = useRoute()
 const router = useRouter()
 
 const login = () => {
   if (email.value === 'aleOrozco@uanl.edu' && password.value === '123') {
     router.push({ name: 'home' })
   } else {
-    error.value = 'Credenciales incorrectas'
+    if (!email.value || !password.value) {
+      error.value = 'Por favor, completa ambos campos.'
+    } else {
+      error.value = 'Credenciales incorrectas'
+    }
   }
 }
 </script>
@@ -23,29 +26,34 @@ const login = () => {
     <img src="@/assets/img/Frame1.svg" alt="Background" class="absolute inset-0 object-cover" />
     <div class="absolute inset-0 bg-white bg-opacity-50 backdrop-blur-sm"></div>
 
-    <div class="w-1/2 max-h-96 bg-opacity-0 p-8 absolute right-14">
-      <h1 class="text-4xl font-bold text-gray-500 mb-6 text-center">Test Forge</h1>
+    <div class="w-1/3 bg-opacity-0 p-8 absolute right-20">
+      <div class="flex items-center">
+        <h1 class="text-4xl font-poppins text-gray-500 text-center">Test Forge</h1>
+        <img src="/src/assets/img/hammer.svg" alt="SVG" class="ml-4" />
+      </div>
       <form @submit.prevent="login" class="bg-opacity-0">
         <div class="mb-6">
-          <label for="username" class="block text-gray-500 text-2xl font-medium"></label>
+          <label for="username" class="block text-gray-500 text-2xl font-poppins mb-2"
+            >Correo</label
+          >
           <input
             v-model="email"
             type="text"
             id="username"
             name="username"
             class="w-full p-2 text-xl border border-transparent rounded-md focus:outline-none focus:ring focus:ring-indigo-300"
-            placeholder="Correo"
           />
         </div>
         <div class="mb-6">
-          <label for="password" class="block text-gray-500 text-2xl font-medium"></label>
+          <label for="password" class="block text-gray-500 text-2xl font-poppins mb-2"
+            >Contraseña</label
+          >
           <input
             v-model="password"
             type="password"
             id="password"
             name="password"
             class="w-full p-2 text-xl border border-transparent rounded-md focus:outline-none focus:ring focus:ring-indigo-300"
-            placeholder="Contraseña"
           />
         </div>
         <div class="text-center">
