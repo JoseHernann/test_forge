8ad6c00b460dfdb7a140cb2454f7281dc96fb109
Alejandra Orozco <nancy.palomerao@uanl.mx>
2023-10-26 22:26:13 -0600

    Aumento del tamaño de los campos de entrada para una mejor legibilidad. Aplicación de un desenfoque suave al fondo opaco para un aspecto más elegante. Ajuste del tamaño y altura del formulario para una presentación más equilibrada.

--- src/views/Auth/LoginPage.vue
+++ src/views/Auth/LoginPage.vue
@@ -20,16 +20,13 @@ const login = () => {
 
 <template>
   <div class="min-h-screen flex items-center justify-center relative">
-    <img
-      src="@/assets/img/Frame1.svg"
-      alt="Background"
-      class="absolute inset-0 w-full h-full object-cover"
-    />
-    <div class="absolute inset-0 bg-white bg-opacity-50"></div>
-    <div class="w-1/4 bg-opacity-0 p-8 rounded-lg shadow-lg absolute right-16">
+    <img src="@/assets/img/Frame1.svg" alt="Background" class="absolute inset-0 object-cover" />
+    <div class="absolute inset-0 bg-white bg-opacity-50 backdrop-blur-sm"></div>
+
+    <div class="w-1/2 max-h-96 bg-opacity-0 p-8 absolute right-14">
       <h1 class="text-4xl font-bold text-gray-500 mb-6 text-center">Test Forge</h1>
       <form @submit.prevent="login" class="bg-opacity-0">
-        <div class="mb-4">
+        <div class="mb-6">
           <label for="username" class="block text-gray-500 text-2xl font-medium"></label>
           <input
             v-model="email"
@@ -53,11 +50,15 @@ const login = () => {
         </div>
         <div class="text-center">
           <button
-            type="submit"
-            class="w-full bg-white p-4 rounded-full text-xl text-gray-500 shadow-lg"
+            class="w-full relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-full group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800"
           >
-            Iniciar sesión
+            <span
+              class="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0"
+            >
+              Iniciar sesión
+            </span>
           </button>
+
           <p class="text-red-500">{{ error }}</p>
         </div>
       </form>
