59ccc6b42dd93485087265e8fb6d0a3e95c183e5
Alejandra Orozco <nancy.palomerao@uanl.mx>
2023-10-30 16:23:57 -0600

    Cambios pequenos en el login

--- src/views/Auth/LoginPage.vue
+++ src/views/Auth/LoginPage.vue
@@ -26,11 +26,9 @@ const login = () => {
 
   <div class="min-h-screen flex items-center justify-center relative">
     <div class="absolute inset-0 bg-white h-full bg-opacity-50 backdrop-blur-sm"></div>
-
     <div class="w-1/2 z-10 p-8 absolute right-20">
       <div class="flex items-center">
         <h1 class="text-7xl ml-40 font-poppins text-gray-500 text-center">Test Forge</h1>
-
         <img src="/src/assets/img/hammer.svg" alt="SVG" class="ml-6" />
       </div>
       <form @submit.prevent="login" class="bg-opacity-0">
